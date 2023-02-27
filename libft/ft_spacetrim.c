ab1927f463c8027f7f5a74e3905a6399f1370f4b
Otavio_Burato <otavioburato42@gmail.com>
2023-02-26 21:06:29 -0300

    fix bug

--- libft/ft_spacetrim.c
+++ libft/ft_spacetrim.c
@@ -6,38 +6,45 @@
 /*   By: oburato <oburato@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:20:45 by oburato           #+#    #+#             */
-/*   Updated: 2023/02/26 20:02:33 by oburato          ###   ########.fr       */
+/*   Updated: 2023/02/26 21:05:56 by oburato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_skip(char *string, int *index)
+{
+	char	ignore;
+
+	if (ft_strchr("\"\'", string[*index]))
+	{
+		ignore = string[*index];
+		while ((ignore != string[*index + 1]) && string[*index + 1])
+			(*index)++;
+		(*index)++;
+	}
+}
+
 char	*ft_spacetrim(char *string)
 {
-	int	index;
-	int	aux;
-	int	len;
+	int		index;
+	int		aux;
+	int		len;
 
 	len = ft_strlen(string);
 	index = 0;
-	while (index < len)
+	while (index++ < len)
 	{
-		if (ft_strchr("\"\'", string[index]))
-			while (!ft_strchr("\"\'", string[index + 1]) && string[index + 1])
-				index++;
+		ft_skip(string, &index);
 		if (string[index] == ' ' && string[index + 1] == ' ')
 		{
 			aux = index;
-			while (aux < (len - 1))
-			{
+			while (aux++ < (len - 1))
 				string[aux] = string[aux + 1];
-				aux++;
-			}
 			string[aux] = '\0';
 			len--;
 			index--;
 		}
-		index++;
 	}
 	return (string);
 }
