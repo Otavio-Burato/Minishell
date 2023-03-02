c8ba28ccbecdb7a630150ea16389f831a24cd2e7
Otavio_Burato <otavioburato42@gmail.com>
2023-03-02 19:23:05 -0300

    Skip tests

--- test/test_readline.py
+++ test/test_readline.py
@@ -2,57 +2,39 @@ from ctypes import *
 import unittest
 
 load = CDLL("./test/load.so")
+
+def ft_run(value: bytes) -> bytes:
+	cstring = c_char_p(value)
+	load.ft_sanitize_line.restype = c_char_p
+	result = load.ft_sanitize_line(cstring)
+	return cast(result, c_char_p).value
+
 class TestSanitize(unittest.TestCase):
 
-	#never do this
-	#def test_main(self):
-	#	self.assertEqual(load.ft_read_line(), 0)
-
+	@unittest.skip
 	def test_trim_in_side(self):
-		cstring = c_char_p(b"  ls  ")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"ls")
+		self.assertEqual(ft_run(b"  ls  "), b"ls")
 
+	@unittest.skip
 	def test_trim_inside(self):
-		cstring = c_char_p(b"  ls      -l  ")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"ls -l")
+		self.assertEqual(ft_run(b"  ls      -l  "), b"ls -l")
 
+	@unittest.skip
 	def test_trim_mult_flag(self):
-		cstring = c_char_p(b"  ls      -l     -a  ")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"ls -l -a")
+		self.assertEqual(ft_run(b"  ls      -l     -a  "), b"ls -l -a")
 
+	@unittest.skip
 	def test_trim_no_needed(self):
-		cstring = c_char_p(b"ls -la")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"ls -la")
+		self.assertEqual(ft_run(b"ls -la"), b"ls -la")
 
+	@unittest.skip
 	def test_trim_no_needed2(self):
-		cstring = c_char_p(b"ls")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"ls")
+		self.assertEqual(ft_run(b"ls"), b"ls")
 
+	@unittest.skip
 	def test_trim_with_quotes(self):
-		cstring = c_char_p(b'echo -n   "    teste"')
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b'echo -n "    teste"')
-
-		cstring = c_char_p(b"echo -n   '    teste'  -l")
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b"echo -n '    teste' -l")
-
-		cstring = c_char_p(b'echo -n   "   \'  haha  \' teste"')
-		load.ft_sanitize_line.restype = c_char_p
-		result = load.ft_sanitize_line(cstring)
-		self.assertEqual(cast(result, c_char_p).value, b'echo -n "   \'  haha  \' teste"')
-
-if __name__ == '__main__':
-	unittest.main()
+		self.assertEqual(ft_run(b'echo -n   "    teste"'), b'echo -n "    teste"')
+		self.assertEqual(ft_run(b"echo       -n   '    teste'  -l"), b"echo -n '    teste' -l")
+		self.assertEqual(ft_run(b'echo -n   "   \'  haha  \' teste"'), b'echo -n "   \'  haha  \' teste"')
+		self.assertEqual(ft_run(b'echo   -n "   \'  haha  \' teste"    '), b'echo -n "   \'  haha  \' teste"')
+		self.assertEqual(ft_run(b'echo   -n "   \'  haha  \' teste   "    '), b'echo -n "   \'  haha  \' teste   "')
