0cd20d1d418ec256904e58fb7e22494a112473e4
MaSander <ma.sander11@gmail.com>
2023-03-04 17:15:07 -0300

    remove commentaries on cd builtins

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oburato <oburato@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: msander <msander@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 17:58:05 by msander           #+#    #+#             */
-/*   Updated: 2023/03/02 19:02:10 by oburato          ###   ########.fr       */
+/*   Updated: 2023/03/04 17:12:10 by msander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@ void	ft_cd(char *args)
 
 	args_splited = ft_split(args, ' ');
 	path = (const char *) args_splited[1];
-	printf("\n\n arg[1]: |%s|", args_splited[1]);
-	printf("\n\n path: |%s|", path);
 	path = ft_strtrim(path, " ");
 	result = chdir(path);
 	ft_free_array(args_splited);
-	printf("chdir: %d\n\n", result);
 	if (result == 0)
 		return ;
 	exit(-1);
--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: msander <msander@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:05:13 by msander           #+#    #+#             */
-/*   Updated: 2023/02/26 21:30:39 by msander          ###   ########.fr       */
+/*   Updated: 2023/03/04 17:11:18 by msander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ void	ft_pwd(char **env)
 
 	path = find_in_env(env, "PWD");
 	path = path + 4;
+	printf("%s\n", path);
 }
